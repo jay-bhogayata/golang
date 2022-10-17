8bb4786c677a8ad9a932c1ffbed3e13f346d1efe
jay bhogayata <jaybhogayata53@gmail.com>
2022-10-17 19:57:32 +0530

    Learn basic array

---
+++ 07-array/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var progLang [6]string
+
+	progLang[0] = "c"
+	progLang[1] = "c++"
+	progLang[2] = "java"
+	progLang[3] = "python"
+	progLang[5] = "Golang"
+
+	fmt.Println(progLang)      // notic space b/w python and golang bcz it initalized 4th index with empty string
+	fmt.Println(len(progLang)) // 6 (declared value)
+	progLang[0] = "c#"         // reassignment
+	fmt.Println(progLang[0])
+	fmt.Println(progLang[0:3])
+
+	var cloudProviders = [4]string{"AWS", "Azure", "Gcp", "IBM"}
+	fmt.Println(cloudProviders)
+
+	countryList := [...]string{"India", "USA", "UK", "Russia"} // compiler automatic determine it's size
+	fmt.Println(countryList)
+}
+
+/*
+=>  array is a collection of data elements of the same type can access each element by an index. fixed size
+
+*/
